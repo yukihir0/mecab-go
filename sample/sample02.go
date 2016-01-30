f582fed455f87c4d73b4e6bb2f8789b7a4f6fba3
yukihir0 <yukihiro.cotori@gmail.com>
2016-01-30 17:07:25 +0900

    add sample

---
+++ sample/sample02.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/yukihir0/mecab-go"
+)
+
+func main() {
+	input := "すもももももももものうち"
+
+	args := mecab.NewArgs()
+	args.DicDir = "/usr/local/Cellar/mecab/0.996/lib/mecab/dic/mecab-ipadic-neologd"
+	parser := mecab.NewParser(args)
+	defer parser.Release()
+
+	nodes, err := parser.Parse(input)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, node := range nodes {
+		if node.Pos == "名詞" && (node.Pos1 == "一般" || node.Pos1 == "固有名詞") {
+			fmt.Println(node.Surface)
+		}
+	}
+}
